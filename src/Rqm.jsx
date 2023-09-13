67c6e4c77b649f4da21277e2ed733194ed7d1ff9
Rakib Shanto <rakibshanto96@gmail.com>
2023-09-13 21:07:26 +0600

    Fixed some alignment issues and changed some fonts

--- src/Rqm.jsx
+++ src/Rqm.jsx
@@ -5,31 +5,31 @@ function Rqm() {
 
   const [state, setState] = useState(prevState);
   return (
-    <div className="container w-50">
-      <div className="row d-flex justify-content-center align-items-center align-content-center flex-wrap vh-100 text-center">
+    <div className="container">
+      <div>
         <div className="col">
           <div id="quote-box">
             <h1 id="text">
               <i className="fa fa-quote-left"></i> {state.quote}
             </h1>
             <h5 id="author">- {state.author}</h5>
-            <a
-              className="button"
-              id="tweet-quote"
-              href="https://twitter.com/intent/tweet"
-              target="_top"
-            >
-              <i class="fa-brands fa-square-x-twitter fa-2xl"></i> Tweet
-            </a>
-            <button
-              id="new-quote"
-              className="button"
-              onClick={() =>
-                setState(data[Math.floor(Math.random() * data.length)])
-              }
-            >
-              New Quote
-            </button>
+            <div className="button">
+              <a
+                id="tweet-quote"
+                href="https://twitter.com/intent/tweet"
+                target="_top"
+              >
+                <i class="fa-brands fa-square-x-twitter fa-2xl"></i>
+              </a>
+              <button
+                id="new-quote"
+                onClick={() =>
+                  setState(data[Math.floor(Math.random() * data.length)])
+                }
+              >
+                New Quote
+              </button>
+            </div>
           </div>
         </div>
       </div>
